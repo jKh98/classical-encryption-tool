d609408e52b7ac194d5765b6dda7a88143a7bb35
jihad khurfan <jkhurfan.jk@gmail.com>
2019-12-01 23:47:03 +0200

    supported distributed exe

--- public/electron.js
+++ public/electron.js
@@ -1,42 +1,31 @@
 const electron = require('electron');
-require('electron-reload');
-const app = electron.app;
+const { app, BrowserWindow } = electron;
 const path = require('path');
 const isDev = require('electron-is-dev');
-const BrowserWindow = electron.BrowserWindow;
 
-let mainWindow;
-
-function createWindow() {
-    mainWindow = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
-
-    mainWindow.loadURL(
-        isDev
-            ? 'http://localhost:3000'
-            : `file://${path.join(__dirname, '../build/index.html')}`,
-    );
-
-    mainWindow.on('closed', () => {
-        mainWindow = null
-    })
-}
-
-app.on('ready', createWindow)
-
-app.on('window-all-closed', () => {
+let mainWindow = null;
+app.on('ready', createWindow);
+app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
         app.quit()
     }
 });
-
-app.on('activate', () => {
+app.on('activate', function () {
     if (mainWindow === null) {
         createWindow()
     }
 });
+function createWindow() {
+    mainWindow = new BrowserWindow({
+        width: 1024,
+        height: 1024,
+        title: "Classic Encryption"
+    });
+    mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+    mainWindow.on('closed', function () {
+        mainWindow = null
+    });
+    mainWindow.on('page-title-updated', function (e) {
+        e.preventDefault()
+    });
+}
