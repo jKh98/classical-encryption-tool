f7b9a9ae1528949d9800f4e97f30559b811aca8e
jihad khurfan <jkhurfan.jk@gmail.com>
2019-12-12 17:40:23 +0200

    added basic code for playfar

--- src/utils/crypoFunctions.js
+++ src/utils/crypoFunctions.js
@@ -95,6 +95,68 @@ export const vigenereDecrypt = (keyword, text) => {
     return decrypt;
 };
 
+
+export const playfairEncrypt = (keysquare, plaintext) => {
+    while (plaintext.length % 2 !== 0) plaintext += "x";
+    let ciphertext = "";
+    let a, b, c, d;
+    for (let i = 0; i < plaintext.length; i += 2) {
+        a = plaintext.charAt(i);
+        b = plaintext.charAt(i + 1);
+        if (a === b) b = "x";
+        let row1 = parseInt(keysquare.indexOf(a) / 5);
+        let col1 = keysquare.indexOf(a) % 5;
+        let row2 = parseInt(keysquare.indexOf(b) / 5);
+        let col2 = keysquare.indexOf(b) % 5;
+        if (row1 === row2) {
+            if (col1 === 4) c = keysquare.charAt(row1 * 5);
+            else c = keysquare.charAt(row1 * 5 + col1 + 1);
+            if (col2 === 4) d = keysquare.charAt(row2 * 5);
+            else d = keysquare.charAt(row2 * 5 + col2 + 1);
+        } else if (col1 === col2) {
+            if (row1 === 4) c = keysquare.charAt(col1);
+            else c = keysquare.charAt((row1 + 1) * 5 + col1);
+            if (row2 === 4) d = keysquare.charAt(col2);
+            else d = keysquare.charAt((row2 + 1) * 5 + col2);
+        } else {
+            c = keysquare.charAt(row1 * 5 + col2);
+            d = keysquare.charAt(row2 * 5 + col1);
+        }
+
+        ciphertext += c + d;
+    }
+    return ciphertext;
+};
+
+export const playfairDecrypt = (keysquare, ciphertext) => {
+    let plaintext = "";
+    for (let i = 0; i < ciphertext.length; i += 2) {
+        let a, b, c, d;
+        a = ciphertext.charAt(i);
+        b = ciphertext.charAt(i + 1);
+        let row1 = parseInt(keysquare.indexOf(a) / 5);
+        let col1 = keysquare.indexOf(a) % 5;
+        let row2 = parseInt(keysquare.indexOf(b) / 5);
+        let col2 = keysquare.indexOf(b) % 5;
+        if (row1 === row2) {
+            if (col1 === 0) c = keysquare.charAt(row1 * 5 + 4);
+            else c = keysquare.charAt(row1 * 5 + col1 - 1);
+            if (col2 === 0) d = keysquare.charAt(row2 * 5 + 4);
+            else d = keysquare.charAt(row2 * 5 + col2 - 1);
+        } else if (col1 === col2) {
+            if (row1 === 0) c = keysquare.charAt(20 + col1);
+            else c = keysquare.charAt((row1 - 1) * 5 + col1);
+            if (row2 === 0) d = keysquare.charAt(20 + col2);
+            else d = keysquare.charAt((row2 - 1) * 5 + col2);
+        } else {
+            c = keysquare.charAt(row1 * 5 + col2);
+            d = keysquare.charAt(row2 * 5 + col1);
+        }
+        plaintext += c + d;
+    }
+    return plaintext;
+};
+
 //export const playfairEncrypt = (keyword, text) => {};
 
 //export const playfairDecrypt = (keyword, text) => {};
