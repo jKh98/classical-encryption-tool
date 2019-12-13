981bdfd58ab334439e6cf1e6772f97db200887dc
jihad khurfan <jkhurfan.jk@gmail.com>
2019-12-13 23:05:59 +0200

    Added function to remove whitespace from strings and another to generate key for playfair

--- src/utils/crypoFunctions.js
+++ src/utils/crypoFunctions.js
@@ -157,6 +157,69 @@ export const playfairDecrypt = (keysquare, ciphertext) => {
     return plaintext;
 };
 
+export const strip = (aString) => {
+
+    var splitString = "";
+
+    for (var item in aString) {
+
+        var letter = aString.charAt(item);
+
+        // ignore whitespace and append to string
+        if (letter.search(/\s|\W|\d/igm) == -1) {
+            splitString += letter;
+        }
+    }
+    return splitString;
+}
+
+export const fillMatrix = () => {
+    var userInput = window.sessionStorage.getItem("kw");
+    var matrix = new Array(25);
+    var matrixIndex = 0;
+    var keyIndex = 0;
+    var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    // strip whitespace
+    userInput = strip(userInput);
+    userInput = userInput.toLowerCase();
+
+    // Fill in the keyword
+    while ( keyIndex < userInput.length ) {
+        var letter = userInput.charAt(keyIndex);
+        if ( matrix.indexOf(letter) === -1 ) {
+            matrix[matrixIndex] = letter;
+            matrixIndex++;
+        }
+        keyIndex++;
+    }
+    // Insert unique letters from the alphabet
+    for (var item in alphabet) {
+        var literal = alphabet.charAt(item);
+
+        //check both uppercase and lowercase letters
+        var letterNotInMatrix = (matrix.indexOf(literal) +
+            matrix.indexOf(literal.toUpperCase()) == -2);
+
+        // if the letter is not in the matrix (-1 + -1)
+        if ( letterNotInMatrix   ) {
+            // Skip i or j if already in matrix
+            if ( (literal === "i" || literal === "I") && (matrix.indexOf("j") === -1 &&
+                matrix.indexOf("J") === -1 ) ) {
+                matrix[matrixIndex] = literal;
+                matrixIndex++;
+            }
+            // replace j with i
+            else if ( (literal === "j" || literal === "J") &&
+                (matrix.indexOf("i") === -1 && matrix.indexOf("I") === -1 ) ) {}
+            else {
+                matrix[matrixIndex] = literal;
+                matrixIndex++;
+            }
+        }
+    }
+    return matrix;
+}
 
 export const hillEncrypt = (keys, plaintext) => {
     keys = keys.split(" ");
